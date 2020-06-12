b8189a45cee15ad8298c89339743ce252d7ec6a1
Loris LEGEAY <loris.legeay.pro@gmail.com>
2020-06-12 15:03:53 +0200

    Premier commit

---
+++ src/main/java/etu/lic/tpsecuritywebcookies/TpSecurityWebCookiesApplication.java
@@ -0,0 +1,13 @@
+package etu.lic.tpsecuritywebcookies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSecurityWebCookiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSecurityWebCookiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/etu/lic/tpsecuritywebcookies/TpSecurityWebCookiesApplicationTests.java
@@ -0,0 +1,13 @@
+package etu.lic.tpsecuritywebcookies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSecurityWebCookiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
